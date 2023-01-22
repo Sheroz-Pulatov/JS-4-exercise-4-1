9c25cb187d9438a8c7d1d4e1def875fad36b3fbc
Sheroz-Pulatov <123168056+Sheroz-Pulatov@users.noreply.github.com>
2023-01-22 19:30:07 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let how_you = prompt('How old are you ?')
+let user_name = prompt('What is your name ?')
+
+if (how_you >= 18 &&user_name[0] === "A" || user_name[0] === "a" ) {
+    alert('WELCOME')
+} else {
+    alert('GO HOME')
+}
+
+
+
